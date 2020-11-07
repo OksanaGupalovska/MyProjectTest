f7e2238ff1e27132ad245c41c3957bcdabb374f9
Oksana Hupalovska <oksanagupalovska@gmail.com>
2020-11-07 16:43:43 +0200

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+import org.apache.commons.io.FileUtils;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Oksana");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+    }
+}
